f003d4491a86b8baf7346b74c92e799084f508de
RileyA[Windows] <praetor57@gmail.com>
2011-03-25 23:56:48 -0700

    Added simple spline class (TODO: actually implement it into the engine itself)

---
+++ src/OgreSubsystem/Spline.cpp
@@ -0,0 +1,45 @@
+//---------------------------------------------------------------------------
+//(C) Copyright Riley Adams 2011
+
+//This file is part of Oryx Engine.
+
+// Oryx Engine is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the license, or
+// (at your option) any later version.
+
+// Oryx Engine is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTEE; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have recieved a copy of the GNU General Public License
+// along with Oryx Engine. If not, see <http://www.gnu.org/licenses/>
+//---------------------------------------------------------------------------
+
+#include "Ogre.h"
+#include "Spline.h"
+#include "OgreConversions.h"
+
+namespace Oryx
+{
+	Spline::Spline()
+	{
+		mSpline = new Ogre::SimpleSpline();
+	}
+
+	Spline::~Spline()
+	{
+		delete mSpline;
+	}
+
+	void Spline::addPoint(Vector3 p)
+	{
+		mSpline->addPoint(convertOgre(p));
+	}
+
+	Vector3 Spline::interpolate(Real t)
+	{
+		return convertOgre(mSpline->interpolate(t));
+	}
+}
